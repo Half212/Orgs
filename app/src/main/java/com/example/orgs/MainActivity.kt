e8f4c78e96111e2c8cad162963fcfddefe11152e
Half212 <aquiles.filho250@gmail.com>
2023-08-01 17:51:12 -0300

    implementando layout

--- app/src/main/java/com/example/orgs/MainActivity.kt
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -1,4 +1,14 @@
 package com.example.orgs
 
-class MainActivity {
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+               setContentView(R.layout.activity_main)
+
+    }
+
 }
