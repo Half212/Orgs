230149f5cce6fcab463746afe44d35ecd278dc96
Half212 <aquiles.filho250@gmail.com>
2023-08-07 10:48:52 -0300

    implementando adpter do recyclerview e refatorando pacotes

--- app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
@@ -1,15 +1,17 @@
-package com.example.orgs
+package com.example.orgs.ui.activity
+
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.orgs.R
+import com.example.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 //        val nome = findViewById<TextView>(R.id.nome)
 //        nome.text = "Cesta de Frutas"
 //
@@ -19,6 +21,8 @@ class MainActivity : Activity() {
 //        val valor = findViewById<TextView>(R.id.valor)
 //        valor.text = "19.99"
 
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = ListaProdutosAdapter()
     }
 
 }
