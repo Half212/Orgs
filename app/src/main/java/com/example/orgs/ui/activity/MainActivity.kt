b1f0fee37329dbc73256a1036c5adb700d955643
Half212 <aquiles.filho250@gmail.com>
2023-08-09 17:03:36 -0300

    implementando um FAB e utilizando intent para transitar entre activities

--- app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
@@ -1,27 +1,20 @@
 package com.example.orgs.ui.activity
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.service.controls.actions.FloatAction
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.orgs.R
 import com.example.orgs.model.Produto
 import com.example.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-//        val nome = findViewById<TextView>(R.id.nome)
-//        nome.text = "Cesta de Frutas"
-//
-//        val descricao = findViewById<TextView>(R.id.descricao)
-//        descricao.text = "Laranja, manga, e maçã"
-//
-//        val valor = findViewById<TextView>(R.id.valor)
-//        valor.text = "19.99"
-
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = ListaProdutosAdapter(
             context = this, produtos = listOf(
@@ -41,6 +34,11 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                     valor = BigDecimal("39.99")
                 ),
         ))
+        val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        fab.setOnClickListener{
+           val intent =  Intent(this,FormularioProdutosActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 
